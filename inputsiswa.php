d41dfd1d529bd04f0e4a5cfadab6a5352866bc45
Alfi Yudin <alfiyudin46@gmail.com>
2019-08-23 08:13:01 +0700

    form

--- inputsiswa.php
+++ inputsiswa.php
@@ -1,29 +1,29 @@
 <?php
 if(isset($_POST['simpan'])){
     $no = $_POST['no'];
-    echo "No Pendaftaran : $no<br>";
+    echo "No Pendaftaran : $no<br><br>";
     $nisn = $_POST['nisn'];
-    echo "NISN : $nisn<br>";
+    echo "NISN : $nisn<br><br>";
     $nama = $_POST['nama'];
-    echo "Nama : $nama<br>";
+    echo "Nama : $nama<br><br>";
     $alamat = $_POST['al'];
-    echo "Alamat : $alamat<br>";
+    echo "Alamat : $alamat<br><br>";
     $tl = $_POST['tl'];
-    echo "Tempat Lahir : $tl<br>";
+    echo "Tempat Lahir : $tl<br><br>";
     $tgl = $_POST['tgl'];
-    echo "Tanggal Lahir : $tgl<br>";
+    echo "Tanggal Lahir : $tgl<br><br>";
     $jenis = $_POST['jk'];
-    echo "Jenis Kelamin : $jenis<br>";
+    echo "Jenis Kelamin : $jenis<br><br>";
     $jurusan = $_POST['jurusan'];
-    echo "Jurusan : $jurusan<br>";
+    echo "Jurusan : $jurusan<br><br>";
     $ayah = $_POST['ayah'];
-    echo "Nama ayah : $ayah<br>";
+    echo "Nama ayah : $ayah<br><br>";
     $paya = $_POST['paya'];
-    echo "Pekerjaan ayah : $paya<br>";
+    echo "Pekerjaan ayah : $paya<br><br>";
     $ibu = $_POST['ibu'];
-    echo "Nama ibu : $ibu<br>";
+    echo "Nama ibu : $ibu<br><br>";
     $pibu = $_POST['pibu'];
-    echo "Pekerjaan ibu : $pibu<br>";
+    echo "Pekerjaan ibu : $pibu<br><br>";
     echo "Hobi : <br>";
     if(isset($_POST['h1'])){
         echo "+ ".$_POST['h1']."<br>";
@@ -40,6 +40,7 @@ if(isset($_POST['simpan'])){
     if(isset($_POST['h5'])){
         echo "+ ".$_POST['h5']."<br>";
     }
+    echo "<br>";
     $agama = $_POST['agama'];
     echo "Agama : $agama<br>";
 }
